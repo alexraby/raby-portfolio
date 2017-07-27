0ce36d43334ce0092e2e5c5b3f4ec84cb2597beb
Alex <araby424@gmail.com>
2017-07-26 23:00:48 -0400

    Custom icons and mobile navbar bad-assery

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,10 @@
 const path = require('path');
 
 module.exports = {
-	entry: {
-		App: "./app/assets/scripts/App.js",
-		Vendor: "./app/assets/scripts/Vendor.js"
-	},
+	entry: "./app/assets/scripts/App.js",
 	output: {
 		path: path.resolve( __dirname, "./app/temp/scripts"),
-		filename: "[name].js"
+		filename: "App.js"
 	},
 
 	module: {
